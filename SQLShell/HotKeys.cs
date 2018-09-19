3e9e14ee105f10aa2e00fdcefa8bdf768b3a0a6f
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-09-19 15:47:14 -0500

    saved work

---
+++ SQLShell/HotKeys.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SQLShell
+{
+    public class HotKeys
+    {
+        public bool ExecuteQuery()
+        {
+            ConsoleKeyInfo input = Console.ReadKey(true);
+
+            if (input.Modifiers == ConsoleModifiers.Control)
+            {
+                if (input.Key == ConsoleKey.Enter)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- SQLShell/Program.cs
+++ SQLShell/Program.cs
@@ -3,27 +3,31 @@ using System.Data.Odbc;
 
 namespace SQLShell
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter DSN that you want to connect to: ");
-            var DSN = Console.ReadLine();
+            Console.Write("Please enter DSN that you want to connect to: ");
 
+            var DSN = Console.ReadLine();
             var DbConnection = new OdbcConnection("DSN=" + DSN);
 
             DbConnection.Open();
 
-            var DbCommand = DbConnection.CreateCommand();
-            DbCommand.CommandText = "select * from tbl_1";
+            Console.Write("SQL-> ");
 
+            var query = Console.ReadLine();
+ 
+
+            var DbCommand = DbConnection.CreateCommand();
+            DbCommand.CommandText = query;
             var DbReader = DbCommand.ExecuteReader();
 
             int fCount = DbReader.FieldCount;
-            Console.Write(":");
             for (int i = 0; i < fCount; i++)
             {
-                String fName = DbReader.GetName(i);
+                var fName = DbReader.GetName(i);
                 Console.Write(fName + ":");
             }
             Console.WriteLine();
