96b842ef8b57fcd6b1ba89ba24f6b77db837c356
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-09-19 11:48:57 -0500

    Successfully connected to local database via odbc

--- SQLShell/Program.cs
+++ SQLShell/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Odbc;
 
 namespace SQLShell
 {
@@ -6,7 +7,42 @@ namespace SQLShell
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter DSN that you want to connect to: ");
+            var DSN = Console.ReadLine();
+
+            var DbConnection = new OdbcConnection("DSN=" + DSN);
+
+            DbConnection.Open();
+
+            var DbCommand = DbConnection.CreateCommand();
+            DbCommand.CommandText = "select * from tbl_1";
+
+            var DbReader = DbCommand.ExecuteReader();
+
+            int fCount = DbReader.FieldCount;
+            Console.Write(":");
+            for (int i = 0; i < fCount; i++)
+            {
+                String fName = DbReader.GetName(i);
+                Console.Write(fName + ":");
+            }
+            Console.WriteLine();
+
+            while (DbReader.Read())
+            {
+                Console.Write(":");
+                for (int i = 0; i < fCount; i++)
+                {
+                    String col = DbReader.GetString(i);
+
+                    Console.Write(col + ":");
+                }
+                Console.WriteLine();
+            }
+
+            DbReader.Close();
+            DbCommand.Dispose();
+            DbConnection.Close();
         }
     }
 }
